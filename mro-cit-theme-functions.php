dfcdda94b8c1b6c4d604d544559bc1f40c2ad2c4
MatiRosero <gekidasa@gmail.com>
2017-09-23 18:40:01 -0600

    initial local commit

---
+++ mro-cit-theme-functions.php
@@ -0,0 +1,11 @@
+<?php
+/*
+Plugin Name: CIT Theme Functions
+Plugin URI: https://github.com/matirosero/cit-theme-functions
+Description: Custom Genesis functions for the Nuts and Bolts Media website.
+Version: 0.1
+Author: Mat Rosero
+Author URI: https://www.matilderosero.com/
+This plugin is released under the GPLv2 license. The images packaged with this plugin are the property of
+their respective owners, and do not, necessarily, inherit the GPLv2 license.
+*/
