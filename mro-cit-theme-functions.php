9b5894c81e4f3f7579bb0119532905ff5affbfe7
MatiRosero <gekidasa@gmail.com>
2017-09-23 22:36:16 -0600

    cmb2 start
    include cmb2, set up includes, show migration info fields as read only

--- mro-cit-theme-functions.php
+++ mro-cit-theme-functions.php
@@ -6,6 +6,46 @@ Description: Custom Genesis functions for the Nuts and Bolts Media website.
 Version: 0.1
 Author: Mat Rosero
 Author URI: https://www.matilderosero.com/
-This plugin is released under the GPLv2 license. The images packaged with this plugin are the property of
-their respective owners, and do not, necessarily, inherit the GPLv2 license.
+This plugin is released under the GPLv2 license. The images packaged with this plugin are the property of their respective owners, and do not, necessarily, inherit the GPLv2 license.
 */
+
+
+/**
+ * Get the CMB2 bootstrap!
+ *
+ * @since 0.1.0
+ */
+if ( file_exists( __DIR__ . '/vendor/cmb2/init.php' ) ) {
+  	require_once __DIR__ . '/vendor/cmb2/init.php';
+} elseif ( file_exists(  __DIR__ . '/vendor/CMB2/init.php' ) ) {
+  	require_once __DIR__ . '/vendor/CMB2/init.php';
+}
+
+
+/**
+ * Load plugin textdomain.
+ *
+ * @since 0.1.0
+ */
+function mro_cit_load_textdomain() {
+	load_plugin_textdomain( 'mro-cit-cpt', false, basename( dirname( __FILE__ ) ) . '/languages' );
+}
+add_action( 'plugins_loaded', 'mro_cit_load_textdomain' );
+
+
+/**
+ * Post types.
+ *
+ * @since 0.1.0
+ */
+require_once( dirname( __FILE__ ) . '/includes/register-cpt.php' );
+
+
+/**
+ * Custom fields (uses CMB2).
+ *
+ * @since 0.1.0
+ */
+require_once( dirname( __FILE__ ) . '/includes/demo-register-custom-fields.php' );
+
+require_once( dirname( __FILE__ ) . '/includes/register-custom-fields.php' );
