07360cc6eb74a17a70d62101e7c7eb93257c01ef
MatiRosero <gekidasa@gmail.com>
2017-11-26 00:58:57 -0600

    profile meta

--- includes/register-custom-fields.php
+++ includes/register-custom-fields.php
@@ -9,6 +9,35 @@ function link_to_old_url( $field_args, $field ) {
 	}
 }
 
+add_action( 'cmb2_admin_init', 'mro_cit_register_board_member_metabox' );
+function mro_cit_register_board_member_metabox() {
+	$prefix = 'mro_cit_board_member_';
+
+	$cmb_demo = new_cmb2_box( array(
+		'id'            => $prefix . 'metabox',
+		'title'         => esc_html__( 'Additional information', 'mro-cit-cpt' ),
+		'object_types'  => array( 
+			'cit_board_members' 
+		), // Post type
+		'context'    => 'normal',
+		'priority'   => 'high',
+	) );
+
+	$cmb_demo->add_field( array(
+		'name'       => esc_html__( 'Position', 'mro-cit-cpt' ),
+		// 'desc'       => esc_html__( 'field description (optional)', 'mro-cit-cpt' ),
+		'id'         => $prefix . 'position',
+		'type'       => 'text',
+	) );
+
+	$cmb_demo->add_field( array(
+		'name' => esc_html__( 'Email', 'mro-cit-cpt' ),
+		// 'desc' => esc_html__( 'field description (optional)', 'mro-cit-cpt' ),
+		'id'   => $prefix . 'email',
+		'type' => 'text_email',
+		// 'repeatable' => true,
+	) );
+}
 
 add_action( 'cmb2_admin_init', 'mro_cit_register_report_metabox' );
 function mro_cit_register_report_metabox() {
@@ -30,12 +59,6 @@ function mro_cit_register_report_metabox() {
 		// 'desc'       => esc_html__( 'field description (optional)', 'mro-cit-cpt' ),
 		'id'         => $prefix . 'download_path',
 		'type'       => 'text',
-		// 'show_on_cb' => 'mro_cit_demo_hide_if_no_cats', // function should return a bool value
-		// 'sanitization_cb' => 'my_custom_sanitization', // custom sanitization callback parameter
-		// 'escape_cb'       => 'my_custom_escaping',  // custom escaping callback parameter
-		// 'on_front'        => false, // Optionally designate a field to wp-admin only
-		// 'repeatable'      => true,
-		// 'column'          => true, // Display field value in the admin post-listing columns
 	) );
 
 
@@ -61,7 +84,6 @@ function mro_cit_register_report_metabox() {
 			'textarea_rows' => 2,
 		),
 	) );
-
 }
 
 
