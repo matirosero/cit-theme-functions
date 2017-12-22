df4bf34b23166cab89e2507a29029f2e598e9ff4
MatiRosero <gekidasa@gmail.com>
2017-12-22 12:16:43 -0600

    cleanup
    remove unneeded

--- mro-cit-theme-functions.php
+++ mro-cit-theme-functions.php
@@ -95,17 +95,6 @@ require_once( dirname( __FILE__ ) . '/includes/eventscalendar-custom-fields.php'
 
 
 /**
- * Registration.
- *
- * @since 0.1.0
- */
-// require_once( dirname( __FILE__ ) . '/includes/registration.php' );
-// require_once( dirname( __FILE__ ) . '/includes/registration-helpers.php' );
-// require_once( dirname( __FILE__ ) . '/includes/emails.php' );
-
-// require_once( dirname( __FILE__ ) . '/includes/registration-user-shortcodes.php' );
-
-/**
  * Caldera.
  *
  * @since 0.1.0
