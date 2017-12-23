22dd171f599b7796663b7aaf8028c62c76b89a41
MatiRosero <gekidasa@gmail.com>
2017-12-22 18:37:21 -0600

    additional readonly fields in event forms

--- includes/caldera-helpers.php
+++ includes/caldera-helpers.php
@@ -2,7 +2,7 @@
 
 //Make fields readonly
 add_filter( 'caldera_forms_field_attributes', function( $attrs, $field ) {
-	if( 'fld_415804' == $field['ID'] || 'fld_9901443' == $field['ID'] || 'fld_415804' == $field['ID'] || 'fld_9901443' == $field['ID'] ) {
+	if( 'fld_415804' == $field['ID'] || 'fld_9901443' == $field['ID'] || 'fld_415804' == $field['ID'] || 'fld_9901443' == $field['ID'] || 'fld_35998' == $field['ID'] || 'fld_8020720' == $field['ID'] ) {
 		$attrs['readonly'] = 'readonly';
 	}
 	return $attrs;
