b9c197b51807ebfcb8c38b167b97dd2cf8431fc3
MatiRosero <gekidasa@gmail.com>
2017-12-23 18:50:11 -0600

    taxonomy for problems

--- includes/register-taxonomies.php
+++ includes/register-taxonomies.php
@@ -13,6 +13,14 @@ add_action( 'init', 'mro_cit_register_tax' );
 /* Register taxonomies */
 function mro_cit_register_tax() {
 
+	$trouble_post_types = array( 
+		'post', 
+		'cit_archive',
+		'cit_unknown',
+		'tribe_events',
+		'cit_past_event',
+		'cit_report' 
+	);
 	$all_post_types = array( 
 		'page', 
 		'post', 
@@ -40,6 +48,40 @@ function mro_cit_register_tax() {
 		'cit_testimonials',
 		'cit_report' 
 	);
+
+	// Add new taxonomy, NOT hierarchical (like tags)
+	$labels = array(
+		'name'                       => _x( 'Problems', 'taxonomy general name', 'mro-cit-functions' ),
+		'singular_name'              => _x( 'Problem', 'taxonomy singular name', 'mro-cit-functions' ),
+		'search_items'               => __( 'Search Problems', 'mro-cit-functions' ),
+		'popular_items'              => __( 'Popular Problems', 'mro-cit-functions' ),
+		'all_items'                  => __( 'All Problems', 'mro-cit-functions' ),
+		'parent_item'                => null,
+		'parent_item_colon'          => null,
+		'edit_item'                  => __( 'Edit Problem', 'mro-cit-functions' ),
+		'update_item'                => __( 'Update Problem', 'mro-cit-functions' ),
+		'add_new_item'               => __( 'Add New Problem', 'mro-cit-functions' ),
+		'new_item_name'              => __( 'New Problem Name', 'mro-cit-functions' ),
+		'separate_items_with_commas' => __( 'Separate problems with commas', 'mro-cit-functions' ),
+		'add_or_remove_items'        => __( 'Add or remove problems', 'mro-cit-functions' ),
+		'choose_from_most_used'      => __( 'Choose from the most used problems', 'mro-cit-functions' ),
+		'not_found'                  => __( 'No problems found.', 'mro-cit-functions' ),
+		'menu_name'                  => __( 'Problems', 'mro-cit-functions' ),
+	);
+
+	$args = array(
+		'hierarchical'          => true,
+		'labels'                => $labels,
+		'show_ui'               => true,
+		'show_admin_column'     => true,
+		'update_count_callback' => '_update_post_term_count',
+		'query_var'             => true,
+		'rewrite'               => array( 'slug' => 'problemas' ),
+	);
+
+	register_taxonomy( 'mro_cit_problem', $trouble_post_types, $args );
+
+
 
 	// Add new taxonomy, NOT hierarchical (like tags)
 	$labels = array(
